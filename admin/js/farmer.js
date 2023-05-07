26c6cb340e94b94a9fc748eb263f019f0a02ffab
Kaarlo Sasiang <sasiang64@gmail.com>
2023-05-08 00:00:00 +0800

    uncomment route-guard

--- admin/js/farmer.js
+++ admin/js/farmer.js
@@ -4,12 +4,35 @@ const url = window.location.href;
 const farmerId = url.slice(url.lastIndexOf("?id=") + 4);
 const mapContainer = document.getElementById("farmerMap");
 const addressField = document.getElementById("address");
+let farmDataYear = new Date().getFullYear();
+console.log(farmDataYear);
 let map;
 
 $(window).on("load", function () {
-  console.log(navigator.geolocation);
   sendGetRequest(`/farmer/recruitement/${farmerId}`).then((res) => {
     console.log(res[0]);
+    sendGetRequest(
+      `/farmer/recruitement/farmer-crop/${farmDataYear}/${res[0]._id}`
+    ).then((crops) => {
+      console.log(crops.result);
+      var donutChartCanvas2 = $("#donutChart2").get(0).getContext("2d");
+      var donutData = {
+        labels: ["Chrome", "IE", "FireFox", "Safari", "Opera", "Navigator"],
+        datasets: [
+          {
+            data: [700, 500, 400, 600, 300, 100],
+            backgroundColor: [
+              "#f56954",
+              "#00a65a",
+              "#f39c12",
+              "#00c0ef",
+              "#3c8dbc",
+              "#d2d6de",
+            ],
+          },
+        ],
+      };
+    });
     _loadMap(res[0].lat, res[0].long);
   });
 });
@@ -17,11 +40,6 @@ $(window).on("load", function () {
 function _loadMap(lat, lng) {
   const latitude = lat;
   const longitude = lng;
-
-  // console.log("Latitude: ", latitude);
-  // console.log("Latitude: ", longitude);
-
-  // console.log(`https://www.google.com/maps/@${latitude},${longitude},20z`);
 
   const coords = [latitude, longitude];
   map = L.map(mapContainer).setView(coords, 17);
@@ -37,3 +55,40 @@ function _loadMap(lat, lng) {
   marker.bindPopup("Farmer Location").openPopup();
   marker.addTo(map);
 }
+
+function loadCharts() {}
+
+$(function () {
+  // var donutChartCanvas2 = $("#donutChart2").get(0).getContext("2d");
+  var donutChartCanvas3 = $("#donutChart3").get(0).getContext("2d");
+  var donutData = {
+    labels: ["Chrome", "IE", "FireFox", "Safari", "Opera", "Navigator"],
+    datasets: [
+      {
+        data: [700, 500, 400, 600, 300, 100],
+        backgroundColor: [
+          "#f56954",
+          "#00a65a",
+          "#f39c12",
+          "#00c0ef",
+          "#3c8dbc",
+          "#d2d6de",
+        ],
+      },
+    ],
+  };
+  var donutOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+  };
+  new Chart(donutChartCanvas2, {
+    type: "doughnut",
+    data: donutData,
+    options: donutOptions,
+  });
+  new Chart(donutChartCanvas3, {
+    type: "doughnut",
+    data: donutData,
+    options: donutOptions,
+  });
+});
