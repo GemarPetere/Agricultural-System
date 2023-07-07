a2f892d729cb16df46f86b4fdb49b14e017e4d6d
Kaarlo Sasiang <sasiang64@gmail.com>
2023-07-08 01:25:11 +0800

    changed api base url

--- admin/js/_hosts.js
+++ admin/js/_hosts.js
@@ -2,7 +2,7 @@ let apiUrl = "";
 let host = document.location.hostname
 
 if(host == "localhost" || host == "127.0.0.1"){
-    apiUrl = "http://localhost:8002/api";
+    apiUrl = "https://recommender-system-api.onrender.com/api";
 }else if(host == "recommender-system.com"){
     apiUrl = ""
 }
